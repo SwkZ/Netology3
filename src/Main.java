7b780979850109c146f85b8ce6172c8581fb05fc
swkz <silvntuspkillac1@gmail.com>
2023-04-03 11:10:17 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonusRubForMile = 20;
+        int cost = 20_000;
+        int milesBonus = cost / bonusRubForMile;
+        System.out.println("Бонус миль за покупку билета составляет: " + milesBonus);
+
+
+    }
+}
